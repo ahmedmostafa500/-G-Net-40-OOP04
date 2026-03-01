7b0d3c20f7f925151bcd39d03804552181ccc32b
ahmedmostafa500 <plmncar500@gmail.com>
2026-03-01 17:00:01 +0200

    question2

--- oop04/Program.cs
+++ oop04/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System.Buffers.Text;
+using System.Numerics;
 using System.Text;
+using System.Xml.Linq;
 
 namespace oop04
 {
@@ -19,13 +21,34 @@ namespace oop04
             // sealed methods
             //Method Overloading
 
-          //  Dynamic binding means the method call is decided at runtime.
-          //  The program decides which method to execute while running, based on the object type.
-          //  When does it happen?
-          // Dynamic binding happens when using:
-          //virtual
-          //override
-          //abstract
+            //  Dynamic binding means the method call is decided at runtime.
+            //  The program decides which method to execute while running, based on the object type.
+            //  When does it happen?
+            // Dynamic binding happens when using:
+            //virtual
+            //override
+            //abstract
+            #endregion
+            #region question2
+            //Q2: What is the difference between method overloading and method overriding?
+            //    Method Overloading means:
+            //    Same method name
+            //    But different parameters
+            //    Inside the same class
+            //    So we create multiple versions of a method.
+            //    Purpose:
+            //    To perform similar actions in different ways
+            //    Happens at:
+            //   Compile Time → (Static Binding)
+
+           // Method Overriding means:
+           //A child class changes the behavior of a method from the parent class.
+           //Conditions:
+           //Must be in inheritance
+           //Parent method must be virtual or abstract
+           //Child method must use override
+           //Happens at:
+           //Runtime → (Dynamic Binding)
         #endregion
         #endregion
     }
